034a5670b1855b5f3e64b20125cf98a1d7da51d1
nhatlam1995 <43503909+nhatlam1995@users.noreply.github.com>
2022-09-02 12:21:04 +0700

    commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './src/App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from '../store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
